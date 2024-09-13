402713279383e306b869dcf50b3797119816fa27
MattG-23451 <thegreenergood24@gmail.com>
2024-09-13 18:55:57 +0700

    Add files via upload

---
+++ JS24/0.js
@@ -0,0 +1,12 @@
+function menu() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
+
+  function menu1(x) {
+    x.classList.toggle("");
+  }
